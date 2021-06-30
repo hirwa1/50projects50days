19e801eab2d17fd4a2bdc32239927d6ebae263f1
NIYIBIZI HIRWA <61378957+hirwa1@users.noreply.github.com>
2021-06-30 13:11:11 +0200

    Update script.js

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -18,6 +18,7 @@ function blurring() {
 }
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+//we
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
